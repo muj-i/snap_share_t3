b5cf9e4df24f8aeba39830e8983adf7007917205
taslims-code <taslimul39@gmail.com>
2023-12-02 18:03:37 +0600

    [ADD]: Custom app bar [ADD]: Some basic theme

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
+import 'package:snap_share_t3/app/common/utils/image_constant.dart';
+import 'package:snap_share_t3/app/common/widgets/appbar/appbar_trailing_icon_button.dart';
 
+import '../../../common/widgets/appbar/appbar_leading_image.dart';
+import '../../../common/widgets/appbar/appbar_title.dart';
+import '../../../common/widgets/appbar/custom_app_bar.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -9,10 +13,7 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
+      appBar: _buildAppbar(),
       body: const Center(
         child: Text(
           'HomeView is working',
@@ -21,4 +22,26 @@ class HomeView extends GetView<HomeController> {
       ),
     );
   }
+}
+
+PreferredSizeWidget _buildAppbar() {
+  return CustomAppBar(
+    centerTitle: true,
+    leadingWidth: 48,
+    leading: AppbarLeadingImage(
+      imagePath: ImageConstant.imgAvatar,
+      margin: const EdgeInsets.only(left: 16, top: 6, bottom: 6),
+      radius: BorderRadius.circular(16),
+    ),
+    title: AppbarTitle(
+      text: 'SociaLive',
+    ),
+    actions: [
+      AppbarTrailingIconButton(
+        color: Colors.red,
+        imagePath: IconConstant.iconNotification,
+        margin: const EdgeInsets.only(left: 16, top: 6, right: 6),
+      ),
+    ],
+  );
 }
