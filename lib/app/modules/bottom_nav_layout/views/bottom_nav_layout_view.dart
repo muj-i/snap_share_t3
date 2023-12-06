367fdc65c2a7407d2751137d32af94ffdc863810
Maruf hasan Rishad <106663161+riShad241@users.noreply.github.com>
2023-12-06 19:00:38 +0600

    Bottom_nav_layout
    Bottom_Nav_layout completed

--- lib/app/modules/bottom_nav_layout/views/bottom_nav_layout_view.dart
+++ lib/app/modules/bottom_nav_layout/views/bottom_nav_layout_view.dart
@@ -1,23 +1,57 @@
 import 'package:flutter/material.dart';
-
-import 'package:get/get.dart';
-
-import '../controllers/bottom_nav_layout_controller.dart';
-
-class BottomNavLayoutView extends GetView<BottomNavLayoutController> {
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:snap_share_t3/app/modules/create_post/views/create_post_view.dart';
+import 'package:snap_share_t3/app/modules/home/views/home_view.dart';
+import 'package:snap_share_t3/app/modules/user_profile/views/user_profile_view.dart';
+import '../../search/views/search_view.dart';
+class BottomNavLayoutView extends StatefulWidget {
   const BottomNavLayoutView({Key? key}) : super(key: key);
+
+  @override
+  State<BottomNavLayoutView> createState() => _BottomNavLayoutViewState();
+}
+
+class _BottomNavLayoutViewState extends State<BottomNavLayoutView> {
+  int _currentIndex = 0;
+  List<Widget> _screen = [
+     HomeView(),
+    SearchView(),
+    CreatePostView(),
+    UserProfileView(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('BottomNavLayoutView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'BottomNavLayoutView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: _screen[_currentIndex],
+      bottomNavigationBar: GNav(
+          tabBorderRadius: 15,
+          gap: 8, // the tab button gap between icon and text
+          color: Colors.black, // unselected icon color
+          activeColor: Colors.blue, // selected icon and text color
+          iconSize: 24, // tab button icon size
+          tabBackgroundColor: Colors.blue.withOpacity(0.1), // selected tab background color
+          padding: EdgeInsets.symmetric(horizontal: 35, vertical: 15), // navigation bar padding
+          tabs: [
+            GButton(
+              icon: Icons.home_outlined,
+            ),
+            GButton(
+              icon: Icons.search,
+            ),
+            GButton(
+              icon: Icons.add,
+            ),
+            GButton(
+              icon: Icons.person_outline,
+            )
+          ],
+        selectedIndex: _currentIndex,
+        onTabChange: (index){
+            setState(() {
+              _currentIndex = index;
+            });
+
+      },
       ),
     );
   }
--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -28,7 +28,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.SPLASH;
+  static const INITIAL = Routes.BOTTOM_NAV_LAYOUT;
 
 
   static final routes = [
